9b08f6bf15c3d2b991083c578b0c252ea2591b5b
redelste <redelste@stevens.edu>
2019-07-10 15:03:34 -0400

    Added comments and explanation

--- ArithmeticProgression.py
+++ ArithmeticProgression.py
@@ -1,18 +1,25 @@
 
 
 def ap(ar):
+    # Diff1: A variable to store the difference between the 
+    #        elements in the array.
     diff1 = 0
+    # NextInt: A variable that is calculated once the differences between numbers
+    #          has been calculated. This is the missing number or "next int".
     nextInt = 0
+    # diff: A variable in which we check the difference between the first and second element.
+    #       This creates a reference value to see what the arithmetic progression is.
     diff = ar[1] - ar[0]
+    # Here we iterate through the input array
     for i in range(len(ar)-1):
+        # Here is when the calculation for diff1 is made. 
         diff1 = ar[i+1] - ar[i]
+        # check to see if the difference between the first and second number is less than the difference
+        # between the other numbers.
         if diff1 > diff:
+            # if it is, we add the lower number to the value as we know the values between arithmetic progressions is always constant.
             nextInt = ar[i] + diff
         elif diff1 < diff:
             nextInt = ar[0] + diff1
     print("THE MISSING NUMBER IS :" + str(nextInt))
 
-
-ap([1, 5, 7, 9, 11])
-ap([2,6,10,14,20])
-ap([2,10,14,16,20,24])
